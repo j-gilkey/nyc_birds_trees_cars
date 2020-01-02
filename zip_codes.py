8e810d0f1961923e6148141fb7b2ce31d3d7c1db
“jackson_gilkey” <jackson.gilkey@gmail.com>
2020-01-02 11:54:44 -0500

    added un zipcode etl

--- zip_codes.py
+++ zip_codes.py
@@ -4,6 +4,18 @@ import tree_data_frame_creation as tree
 
 search = SearchEngine(simple_zipcode=True)
 
-all_zip_codes = list(tree.create_tree_dataframe().zipcode.unique())
+def parse_zips():
+    all_zip_codes = list(tree.create_tree_dataframe().zipcode.unique())
 
-print(all_zip_codes)
+    #print(all_zip_codes)
+
+    for zip in all_zip_codes:
+        zip_data = search.by_zipcode(zip)
+        #print(zip_data)
+        if zip_data.median_home_value:
+            zip_tuple = (int(zip), int(zip_data.median_home_value), int(zip_data.median_household_income), zip_data.land_area_in_sqmi)
+            print(zip_tuple)
+            mysql_functions.insert_zip(zip_tuple)
+
+
+parse_zips()
